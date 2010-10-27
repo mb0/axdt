9ae938e8f70fde8f7684a9862e75414b1b1c1de9
mb0 <mb0@mb0.org>
2010-10-27 19:51:33 +0200

    added missing license header for avm projects

--- avm/avm-ui/src/main/java/org/axdt/avm/ui/proposal/IAvmProposalProvider.java
+++ avm/avm-ui/src/main/java/org/axdt/avm/ui/proposal/IAvmProposalProvider.java
@@ -1,4 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Martin Schnabel <mb0@mb0.org>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package org.axdt.avm.ui.proposal;
+
 import org.axdt.avm.model.AvmDefinition;
 import org.axdt.avm.model.AvmType;
 import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
